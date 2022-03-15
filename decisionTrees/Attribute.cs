ae18f5a6fa32c620e3298acabdadebbe67b1bddb
flapo <filap4@gmail.com>
2022-03-15 12:09:59 +0100

    code refactor

--- decisionTrees/Attribute.cs
+++ decisionTrees/Attribute.cs
@@ -3,20 +3,20 @@
 internal class Attribute
 {
     public int Index { get; }
-    public IEnumerable<object> Values { get; }
     public Dictionary<object, int> Classes { get; }
-    public double InfoT { get; private set; }
+    public double InfoT { get; }
     public double InfoAnT { get; private set; }
     public double GainAnT { get; private set; }
     public double SplitInfoAnT { get; private set; }
     public double GainRatioAnT { get; private set; }
 
+    private readonly IEnumerable<object> _values;
     private readonly double _objects;
 
     public Attribute(int index, object[] values, double infoT)
     {
         Index = index;
-        Values = values ?? throw new ArgumentNullException(nameof(values));
+        _values = values ?? throw new ArgumentNullException(nameof(values));
         InfoT = infoT;
         Classes = values.GroupBy(x => x).ToDictionary(x => x.Key,
             y => y.Select(g => g).Sum(s => 1));
@@ -26,7 +26,7 @@ internal class Attribute
     internal async Task Calculate(object[] decisions)
     {
         var probabilities = Classes.Values.Select(v => v / _objects).ToArray();
-        InfoAnT = await Math.Info(probabilities, Values.ToArray(), decisions);
+        InfoAnT = await Math.Info(probabilities, _values.ToArray(), decisions);
         GainAnT = await Math.Gain(InfoT, InfoAnT);
         SplitInfoAnT = await Math.SplitInfo(probabilities);
         GainRatioAnT = await Math.GainRatio(GainAnT, SplitInfoAnT);
