76863a40b934d6f10404d64563ee834bce594e5e
flapek <46303285+flapek@users.noreply.github.com>
2022-05-09 23:53:35 +0200

    update algorithm

--- decisionTrees/CommandLineOptions.cs
+++ decisionTrees/CommandLineOptions.cs
@@ -6,4 +6,7 @@ internal sealed class CommandLineOptions
 {
     [Option('d', "data", Required = true, HelpText = "Path to file with data")]
     public string? Path { get; set; }
+    [Option('s', "separator", Required = false, HelpText = "Separator", Default = ' ')]
+    public char Separator { get; set; }
+
 }
--- decisionTrees/Program.cs
+++ decisionTrees/Program.cs
@@ -1,77 +1,98 @@
 ﻿using CommandLine;
 using System.Diagnostics;
+using System.Text;
 using decisionTrees;
-using Attribute = decisionTrees.Attribute;
 using Math = decisionTrees.Math;
 
+StringBuilder stringBuilder = new();
+var tabs = 0;
 await Parser.Default.ParseArguments<CommandLineOptions>(args)
     .MapResult(async args =>
     {
-        try
-        {
-            var stopwatch = Stopwatch.StartNew();
-            using StreamReader reader = new(args.Path ?? string.Empty);
-
-            var matrix = await ReadFileAsync(reader);
-            var attributes = await CountAttributes(matrix);
-
-            var we = attributes.Where(x => x.Index != attributes.Count - 1).FirstOrDefault(x =>
-                System.Math.Abs(x.GainRatioAnT - attributes.Max(attribute => attribute.GainRatioAnT)) < 0.000001);
-
-            
-            
-            stopwatch.Stop();
-            Console.WriteLine("Elapsed time in milliseconds: {0}", stopwatch.ElapsedMilliseconds);
-            return 0;
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-            return -3;
-        }
+        var stopwatch = Stopwatch.StartNew();
+        using StreamReader reader = new(args.Path ?? string.Empty);
+        var data = await ReadFileAsync(reader, args.Separator);
+        await BuildTree(data);
+        Console.WriteLine(stringBuilder.ToString());
+        stopwatch.Stop();
+        Console.WriteLine("Elapsed time in milliseconds: {0}", stopwatch.ElapsedMilliseconds);
+        return 0;
     }, _ => Task.FromResult(-1));
 
-async ValueTask<List<Attribute>> CountAttributes(IReadOnlyList<object[]> matrix)
-{
-    List<Attribute> result = new();
-    var attributesCount = matrix[0].Length;
-    var decisions = matrix.Select(x => x[attributesCount - 1]).ToArray();
-
-    var entropy = await Math.Info(decisions.GroupBy(o => o)
-        .ToDictionary(grouping => grouping.Key, grouping => grouping.Select(o => o).Sum(_ => 1))
-        .Values.Select(i => (double) i / decisions.Length)
-        .ToArray());
-
-    for (var i = 0; i < attributesCount; i++)
-    {
-        var attribute = new Attribute(i, matrix.Select(x => x[i]).ToArray(), entropy);
-        if (i != attributesCount - 1) await attribute.Calculate(decisions);
-        result.Add(attribute);
-    }
-
-    return result;
-}
-
-async ValueTask<List<object[]>> ReadFileAsync(StreamReader reader)
+async ValueTask<List<object[]>> ReadFileAsync(StreamReader reader, char separator)
 {
     var result = Enumerable.Empty<object[]>().ToList();
 
     while (!reader.EndOfStream)
-        result.Add((await reader.ReadLineAsync() ?? "").Trim().Split(' ').Select(s => s as object).ToArray());
+        result.Add((await reader.ReadLineAsync() ?? "").Trim().Split(separator).Select(s => s as object).ToArray());
 
     return result;
 }
 
-Task BuildTree(int index, int startedIndex, IReadOnlyList<Attribute> attributes)
+async Task BuildTree(IReadOnlyList<object[]> data)
 {
-    if (attributes.FirstOrDefault(x=>x.Index == startedIndex)!.GainRatioAnT != 0)
+    var decisions = data.Select(x => x.Last()).GroupBy(x=> x).Select(x=>x.Key).ToArray();
+    if (decisions.Length != 1)
     {
-        
+        ++tabs;
+        var idx = await FindTheBest(data);
+        stringBuilder.Append($"Attribute: {idx}");
+        var sort = await Sort(idx, data);
+        foreach (var d in sort)
+        {
+            stringBuilder.Append('\n');
+            for (var i = 0; i < tabs; i++)
+                stringBuilder.Append('\t');
+            stringBuilder.Append($"{d[0][idx]} -> ");
+
+            await BuildTree(d);
+        }
+
+        --tabs;
     }
     else
-    {
-        
-    }
+        stringBuilder.Append($"Decision: {decisions[0]}");
+}
+
+Task<List<object[][]>> Sort(int idx, IReadOnlyList<object[]> data)
+{
+    var attribute = data.Select(el => el[idx]);
+    var result = new List<object[][]>();
     
-    return Task.CompletedTask;
+    foreach (var uniq in attribute.GroupBy(x => x))
+    {
+        var temp = new List<object[]>();
+        foreach (var d in data)
+            if (d[idx].Equals(uniq.Key))
+                temp.Add(d);
+        result.Add(temp.ToArray());
+    }
+
+    return Task.FromResult(result);
+}
+
+ValueTask<int> FindTheBest(IReadOnlyList<object[]> data)
+{
+    var temporary = Enumerable.Range(0, data[0].Length - 1).Select(idx => Calculate(idx, data).Result).ToArray();
+    var max = temporary.Max();
+    return ValueTask.FromResult(Array.IndexOf(temporary, max));
+}
+
+async ValueTask<double> Calculate(int idx, IReadOnlyList<object[]> data)
+{
+    var values = data.Select(el => el[idx]).ToArray();
+    var classes = values.GroupBy(x => x).ToDictionary(x => x.Key,
+        y => y.Select(g => g).Sum(_ => 1));
+    var decisions = data.Select(el => el.LastOrDefault()!).ToArray();
+
+    var probabilities = classes.Values.Select(v => v / (double) values.Length).ToArray();
+    var infoT = await Math.Info(decisions.GroupBy(o => o)
+        .ToDictionary(grouping => grouping.Key, grouping => grouping.Select(o => o).Sum(_ => 1))
+        .Values.Select(i => (double) i / decisions.Length)
+        .ToArray());
+    var infoAnT = await Math.Info(probabilities, values, decisions);
+    var gainAnT = await Math.Gain(infoT, infoAnT);
+    var splitInfoAnT = await Math.SplitInfo(probabilities);
+
+    return await Math.GainRatio(gainAnT, splitInfoAnT);
 }
