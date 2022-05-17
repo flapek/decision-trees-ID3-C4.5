46a0a052b597e807a3ad5f4e1f68ff5a3fd31833
flapek <46303285+flapek@users.noreply.github.com>
2022-05-17 12:27:51 +0200

    add building tree and displaing

--- decisionTrees/Program.cs
+++ decisionTrees/Program.cs
@@ -1,21 +1,21 @@
 ﻿using CommandLine;
 using System.Diagnostics;
-using System.Text;
 using decisionTrees;
 using Math = decisionTrees.Math;
 
-StringBuilder stringBuilder = new();
-var tabs = 0;
+ int tabs = 0;
 await Parser.Default.ParseArguments<CommandLineOptions>(args)
     .MapResult(async args =>
     {
         var stopwatch = Stopwatch.StartNew();
         using StreamReader reader = new(args.Path ?? string.Empty);
-        var data = await ReadFileAsync(reader, args.Separator);
-        await BuildTree(data);
-        Console.WriteLine(stringBuilder.ToString());
+        // var (trainingSet, testSet) = await SplitData(ReadFileAsync(reader, args.Separator));
+
+        var node = new Node(await ReadFileAsync(reader, args.Separator));
+        await BuildTree(node);
+        await DisplayTree(node);
         stopwatch.Stop();
-        Console.WriteLine("Elapsed time in milliseconds: {0}", stopwatch.ElapsedMilliseconds);
+        Console.WriteLine("\nElapsed time in milliseconds: {0}", stopwatch.ElapsedMilliseconds);
         return 0;
     }, _ => Task.FromResult(-1));
 
@@ -29,28 +29,60 @@ async ValueTask<List<object[]>> ReadFileAsync(StreamReader reader, char separato
     return result;
 }
 
-async Task BuildTree(IReadOnlyList<object[]> data)
+async ValueTask<(IEnumerable<object[]> trainingSet, IEnumerable<object[]> testSet)> SplitData(ValueTask<List<object[]>> readFileAsync)
 {
-    var decisions = data.Select(x => x.Last()).GroupBy(x => x).Select(x => x.Key).ToArray();
-    var (idx, ratio) = await FindTheBest(data);
+    var data = await readFileAsync;
+
+    var min = (int) (data.Count * 0.1);
+    min = min == 0 ? 1 : min;
+    var max = (int) (data.Count * 0.3);
+    var testSet = data.OrderBy(_ => Random.Shared.Next()).Take(Random.Shared.Next(min, max)).ToList();
+    foreach (var x in testSet)
+        data.Remove(x);
+
+    return (data, testSet);
+}
+
+async Task BuildTree(Node node)
+{
+    var decisions = node.Data.Select(x => x.Last()).GroupBy(x => x).Select(x => x.Key).ToArray();
+    var (idx, ratio) = await FindTheBest(node.Data);
     if (ratio != 0)
     {
-        ++tabs;
-        stringBuilder.Append($"Attribute: {idx}");
-        var sort = await Sort(idx, data);
+        node.Attribute = idx;
+        var sort = await Sort(idx, node.Data);
         foreach (var d in sort)
         {
-            stringBuilder.Append('\n');
-            for (var i = 0; i < tabs; i++) stringBuilder.Append('\t');
-            stringBuilder.Append($"{d[0][idx]} -> ");
-
-            await BuildTree(d);
+            var child = new Node(d)
+            {
+                Value = d[0][idx]
+            };
+            node.Childs.Add(child);
+            await BuildTree(child);
         }
+    }
+    else
+        node.Decision = decisions[0];
+}
 
+async Task DisplayTree(Node tree)
+{
+    if (tree.Childs.Count != 0)
+    {
+        ++tabs;
+        Console.Write($"Attribute: {tree.Attribute}");
+        foreach (var child in tree.Childs)
+        {
+            Console.Write('\n');
+            for (var i = 0; i < tabs; i++) Console.Write('\t');
+            Console.Write($"{child.Value} -> ");
+
+            await DisplayTree(child);
+        }
         --tabs;
     }
     else
-        stringBuilder.Append($"Decision: {decisions[0]}");
+        Console.Write($"Decision: {tree.Decision}");
 }
 
 Task<List<object[][]>> Sort(int idx, IReadOnlyList<object[]> data)
@@ -93,4 +125,19 @@ async ValueTask<double> Calculate(int idx, IReadOnlyList<object[]> data)
     var splitInfoAnT = await Math.SplitInfo(probabilities);
 
     return await Math.GainRatio(gainAnT, splitInfoAnT);
+}
+
+internal class Node
+{
+    public IReadOnlyList<object[]> Data { get; }
+    public List<Node> Childs { get; }
+    public object? Attribute { get; set; }
+    public object? Decision { get; set; }
+    public object? Value { get; set; }
+
+    public Node(IReadOnlyList<object[]> data)
+    {
+        Data = data;
+        Childs = new List<Node>();
+    }
 }
